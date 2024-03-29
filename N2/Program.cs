d4b004db1e902d37fe0c57a69ebbb80f26878728
vardan <96.vardan.khachatryan@gmail.com>
2024-03-29 15:00:38 +0400

    new

---
+++ N2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2:
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+AckFunction(m,n);
+void AckFunction(int m, int n)
+{
+    Console.Write(Ack(m, n)); 
+}
+int Ack(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Ack(m - 1, 1);
+    }
+    else
+    {
+        return (Ack(m - 1, Ack(m, n - 1)));
+    }
+}
---
+++ N3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: 
+//Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+ static void print(int [] A, int n) 
+    { 
+      if (n > 0) 
+      { 
+      	Console.Write(A[n] + " "); 
+      	print(A, n - 1); 
+      } 
+      else Console.WriteLine(A[0]); 
+    } 
+      int [] num = {1, 2, 5, 0, 10, 34}; 
+      print(num, num.Length - 1); 
+    
